cec98cb3ec7bae1cd2fb01e2a0559bb84c3f540e
shaneeiesen <shanetech.it@gmail.com>
2024-01-22 18:22:13 -0800

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#include <stdlib.h>
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 
 /**
 * init_dog -  initializes a variable of type struct dog
@@ -8,13 +8,20 @@
 * @age: age to initialize
 * @owner: owner to initialize
 */
-
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-if (d == NULL)
-d = malloc(sizeof(struct dog));
-d->name = name;
-d->age = age;
-d->owner = owner;
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
