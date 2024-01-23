507c82478ef22b1d4950ab46d50e143edb8ce8f7
shaneeiesen <shanetech.it@gmail.com>
2024-01-23 14:25:12 -0800

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,1 +1,1 @@
-This is project 0x0E-structures_typedef readme file
+0x0E. C - Structures, typedef
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,38 +1,20 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H_
+#define DOG_H_
 
+/**
+	 * struct dog - a dog's basic info
+	 * @name: First member
+	 * @age: Second member
+	 * @owner: Third member
+	 *
+	 * Description: Longer description
+	 */
 
-   /**
-    * struct dog - dog class
-    *
-    * @name: Name of the dog
-    *
-    * @age: Age of the dog
-    *
-    * @owner: Owner of the dog
-    */
-
-
-   struct dog
+struct dog
 {
-   char *name;
-   float age;
-   char *owner;
+	char *name;
+	float age;
+	char *owner;
+
 };
-
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-
-typedef struct dog dog_t;
-
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-
-void free_dog(dog_t *d);
-
-
 #endif
-
